27d17dbbb0234f85bb5bfb197153c0d60556d70e
Jonathan Francisco <jdavidfrancisco@gmail.com>
2015-05-14 09:47:05 -0700

    updated chunk class!

--- Cubit/src/cubit/Chunk.java
+++ Cubit/src/cubit/Chunk.java
@@ -39,5 +39,34 @@ public class Chunk {
         glPopMatrix();
     }
     
+    public void rebuildMesh (float startX, float startY, float startZ) {
+        VBOColorHandle = glGenBuffers();
+        VBOVertexHandle = glGenBuffers();
+        FloatBuffer VertexPositionData = BufferUtils.createFloatBuffer ((
+            CHUNK_SIZE *CHUNK_SIZE * CHUNK_SIZE) *6 * 12);
+        FloatBuffer VertexColorData = BufferUtils.createFloatBuffer((CHUNK_SIZE *
+                    CHUNK_SIZE * CHUNK_SIZE) * 6 * 12);
+        for (float x = 0; x < CHUNK_SIZE; x++) {
+            for (float z = 0; z < CHUNK_SIZE; z++) {
+                for (float y = 0; y < CHUNK_SIZE; y++) {
+                    VertexPositionData.put(createCube((float)(startX + x 
+                            * CUBE_LENGTH), (float)(y * CUBE_LENGTH + (int)
+                                    (CHUNK_SIZE * .8)), (float)(startZ + z * 
+                                            CUBE_LENGTH)));
+                    VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int)x][(int) y][(int) z])));
+                }
+            }
+        }
+        
+        VertexColorData.flip();
+        VertexPositionData.flip();
+        glBindBuffer(GL_ARRAY_BUFFER, VBOVertexHandle);
+        glBufferData(GL_ARRAY_BUFFER, VertexPositionData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOColorHandle);
+        glBufferData(GL_ARRAY_BUFFER, VertexColorData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    
 }
     
